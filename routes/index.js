9ecb3e89b6d7bbc024d6f54041d092d1e0e57224
Luis Amez <luis.amez.vitoria@gmail.com>
2018-02-08 10:19:25 +0100

    Add upload route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const multer = require('multer');
 const router = express.Router();
+const Picture = require('../models/picture');
+
+const upload = multer({ dest: './public/uploads/' });
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+
+router.post('/upload', upload.single('photo'), (req, res, next) => {
+  const pic = new Picture({
+    name: req.body.name,
+    path: `/uploads/${req.file.filename}`,
+    originalName: req.file.originalname
+  });
+
+  pic.save((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
